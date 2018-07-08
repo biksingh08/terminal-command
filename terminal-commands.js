6736845592234127d1cda0b85f6b4dc5fa84ecc0
Bikramjit Singh <bigsingh@ucdavis.edu>
2018-07-08 11:14:44 -0700

    added touch amd mkdir modules

---
+++ terminal-commands.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+
+module.exports.ls = () => {
+  fs.readdir('./', (err, files) => {
+    const filesToString = files.reduce((acc, file) => {
+      return `${acc} ${file} `;
+    }, '');
+
+    console.log(filesToString);
+  });
+};
+
+module.exports.touch = (userInput) => {
+  fs.writeFile(`${userInput}`, 'new file', function (err) {
+    if (err)
+        return console.log(err);
+    console.log(`${userInput} created`);
+});
+};
+
+module.exports.mkdir = (userInput) => {
+  fs.mkdir(`${userInput}`, function (err) {
+    if (err)
+        return console.log(err);
+    console.log(`${userInput} created`);
+});
+};
